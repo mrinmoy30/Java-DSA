b868a1f372ccb12614ba0dbf40e901f99898299c
mrinmoy30 <sonowalmrinmoy8@gmail.com>
2023-10-17 11:41:23 +0530

    Testing

---
+++ Operators.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Operators {
+    public static void main(String[] args) {
+     /*  int x = 100;
+        long y = x;
+       
+        //Typecasting
+        long x = 1000;
+        int y = (int)x ;   //Syntax is necessary here
+     */
+        
+        //Reading Inputs
+        Scanner scn = new Scanner(System.in);
+            int x = scn.nextInt();
+            int y = scn.nextInt(); 
+            
+            
+            System.out.println(x);
+            System.out.println(y);
+    }
+}
---
+++ simple.java
@@ -0,0 +1,23 @@
+public class simple {
+
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(7*12);
+        System.out.println(10/5);
+        System.out.println("Mrinmoy Sonowal");
+        System.out.println(100+9+15+"Hello");
+       /*
+       int temp;
+        temp = 20;
+        
+        //or
+        int temp = 20;
+        */
+        int temp;
+        temp=200;
+        temp=100;
+        System.out.println(temp);
+        temp=500;
+        System.out.println(temp);
+    }
+}
