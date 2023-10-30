def3dbf9349d7626f59d3591c4f8851dc113e16f
mrinmoy30 <sonowalmrinmoy8@gmail.com>
2023-10-30 20:32:24 +0530

    ok

--- L8Array1.java
+++ L8Array1.java
@@ -1,25 +1,71 @@
 import java.util.Scanner;
 public class L8Array1 {
-  // Java Program to find sum of elements in a given array
-	static int arr[] = { 12, 3, 4, 15 };
+//   // Java Program to find sum of elements in a given array
+// // 	static int arr[] = { 12, 3, 4, 15 };
 
-	// method for sum of elements in an array
-	static int sum()
-	{
-		int sum = 0; // initialize sum
-		int i;
+// // 	// method for sum of elements in an array
+// // 	static int sum()
+// // 	{
+// // 		int sum = 0; // initialize sum
+// // 		int i;
 
-		// Iterate through all elements and add them to sum
-		for (i = 0; i < arr.length; i++)
-			sum += arr[i];
+// // 		// Iterate through all elements and add them to sum
+// // 		for (i = 0; i < arr.length; i++)
+// // 			sum += arr[i];
 
-		return sum;
-	}
+// // 		return sum;
+// // 	}
 
-	// Driver method
-	public static void main(String[] args)
-	{
-		System.out.println("Sum of given array is "
-						+ sum());
-	}
+// // 	// Driver method
+// // 	public static void main(String[] args)
+// // 	{
+// // 		System.out.println("Sum of given array is "
+// // 						+ sum());
+// // 	}
+// // }
+
+
+
+// //Read an array of length N and print the max of all the elements
+
+// public static void main(String[] args) {
+// 	Scanner scn = new Scanner(System.in);
+// 	int n = scn.nextInt();
+//   int arr[] = new int[n];
+
+  
+
+//   for(int i=0; i<n; i++) {
+//     arr[i]= scn.nextInt();
+  
+//   }
+
+
+//   int max= Integer.MIN_VALUE;
+
+//   for(int i=0; i<n; i++) {
+//     if(arr[i]>max) {
+//       max=arr[i];
+//     } else {
+
+//     }
+//   }
+//   System.out.println(max);
+// }
+// }
+
+
+
+public static void main(String[] args) {
+  int a=10;
+  int b=20;
+  swap(a,b);
+  System.out.println(a);
+  System.out.print(b);
+}
+public static void swap(int a, int b) {
+  int temp =a;
+  a=b;
+  b=temp;
+}
 }
--- Lecture3.java
+++ Lecture3.java
@@ -1,28 +1,28 @@
-import java.util.Scanner;
-public class Lecture3 {
-    public static void main(String[] args) {
-     /*
-        int a = 100000;
-        int b = 100000;
-        long c = (long)(x*y);
-        System.out.println(c);
-     //This is wrong, ALU is still int*int
+// import java.util.Scanner;
+// public class Lecture3 {
+//     public static void main(String[] args) {
+//      /*
+//         int a = 100000;
+//         int b = 100000;
+//         long c = (long)(x*y);
+//         System.out.println(c);
+//      //This is wrong, ALU is still int*int
      
-long d = (long)(a)*b ; //here, first a is converted to long and then multiplication is done, and wkt, long*int=long
-        //also, keep in mind the parenthesis- (long)(a) 
-        System.out.println(d);
+// long d = (long)(a)*b ; //here, first a is converted to long and then multiplication is done, and wkt, long*int=long
+//         //also, keep in mind the parenthesis- (long)(a) 
+//         System.out.println(d);
         
-        System.out.println(15%4);
-        System.out.println(16.0/3);   //for answer in decimals, otherwise 16/3 will round of to give output 5 only
+//         System.out.println(15%4);
+//         System.out.println(16.0/3);   //for answer in decimals, otherwise 16/3 will round of to give output 5 only
         
-        //x % N ----remainder will be between 0 to N-1
-        */
-      Scanner scn = new Scanner(System.in);
-      int age = scn.nextInt();
-      if (age>=18) {
-        System.out.println("Eligible");
-      }
-        }
+//         //x % N ----remainder will be between 0 to N-1
+//         */
+//       Scanner scn = new Scanner(System.in);
+//       int age = scn.nextInt();
+//       if (age>=18) {
+//         System.out.println("Eligible");
+//       }
+//         }
         
         
-    }
+//     }
